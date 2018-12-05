b9ac5b38cec432eebe0d8889ec79921d63583bc2
yutong wang <31590637+elvawyt@users.noreply.github.com>
2018-12-05 12:31:29 -0500

    testing

--- imports/ui/Convert.test.js
+++ imports/ui/Convert.test.js
@@ -1,22 +1,29 @@
 import {Meteor} from 'meteor/meteor';
 import React from 'react';
-import expect from 'expect';
+import expect, {createSpy} from 'expect';
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-15';
 import {mount} from 'enzyme';
 import Converter from './../ui/Converter';
+import sinon from 'sinon';
 Enzyme.configure({ adapter: new Adapter() });
 
 
 if(Meteor.isClient){
     describe('Converter',function(){
         it('button text should be to',function(){
-            const wrapper = mount(<Converter/>)
+            const wrapper = mount(<Converter handleClick = {()=>{}}/>)
             const buttontext = wrapper.find('button').text();
             expect(buttontext).toBe('to');
         });
         it('should call the function',function(){
             const spy = expect.createSpy();
+            const wrapper = mount(<Converter handleClick = {spy}/>);
+            wrapper.find('button').simulate("click");      
+            expect(spy).toHaveBeenCalled();
+
+    
         });
+        
     });
 }
